da72156aa6a475870ce2d5a349acc41ebd2b624b
abbyMikoto <abbigailmfarr@gmail.com>
2026-08-12 19:30:23 -0700

    AF - confirming timeline updatetes

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
     client_id: clientId,
     scope: "user-top-read",
     redirect_uri:
-      "http://127.0.0.1:3000/api/auth/callback",
+      "http://127.0.0.1:3000/api/auth/callback", 
   });
 
   return Response.redirect(
